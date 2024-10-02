41e49424e7c717d938c574bfdbd7b765fbd6dfc9
Dmitrii <dimterion59@gmail.com>
2024-10-02 11:58:38 +0200

    add prop types, react scroll & initial header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,15 @@
+import PropTypes from "prop-types";
+import { Link as LinkScroll } from "react-scroll";
 import headerLogo from "../assets/header/header_logo.png";
+
+const NavLink = ({ title }) => (
+  <LinkScroll
+    to={title}
+    className="base-bold max-lg:h5 cursor-pointer uppercase text-p4 transition-colors duration-500 hover:text-p1 max-lg:my-4"
+  >
+    {title}
+  </LinkScroll>
+);
 
 const Header = () => {
   return (
@@ -7,16 +18,41 @@ const Header = () => {
         <a className="z-2 flex-1 cursor-pointer lg:hidden">
           <img src={headerLogo} width={115} height={55} alt="Header logo" />
         </a>
-        <div className="w-full border-2 border-amber-400">
-          <nav>
-            <ul className="flex max-lg:block max-lg:px-12">
-              <li className=""></li>
-            </ul>
-          </nav>
+        <div className="w-full max-lg:fixed max-lg:left-0 max-lg:top-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+          <div className="sidebar-before w-full max-lg:relative max-lg:flex max-lg:min-h-screen max-lg:flex-col max-lg:overflow-hidden max-lg:p-6 max-md:px-4">
+            <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+              <ul className="flex max-lg:block max-lg:px-12">
+                <li className="nav-li">
+                  <NavLink title="features" />
+                  <div className="dot" />
+                  <NavLink title="pricing" />
+                </li>
+                <li className="nav-logo">
+                  <LinkScroll to="hero">
+                    <img
+                      src={headerLogo}
+                      width={160}
+                      height={55}
+                      alt="Header logo"
+                    />
+                  </LinkScroll>
+                </li>
+                <li className="nav-li">
+                  <NavLink title="faq" />
+                  <div className="dot" />
+                  <NavLink title="download" />
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
   );
+};
+
+NavLink.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Header;
