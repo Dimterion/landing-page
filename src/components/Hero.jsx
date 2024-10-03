a8ce05b401a3ef270f4bcbdf48dd55386cf7e4b7
Dmitrii <dimterion59@gmail.com>
2024-10-03 11:32:13 +0200

    add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import { Element, Link as LinkScroll } from "react-scroll";
+
+const Hero = () => {
+  return (
+    <section className="relative border-2 border-amber-400 pb-40 pt-60 max-lg:pb-36 max-lg:pt-52 max-md:pb-32 max-md:pt-36">
+      <Element name="hero">
+        <div className="container">
+          <div className="relative z-2 max-w-512 max-lg:max-w-388">
+            <div className="caption small-2 uppercase text-p3">Lorem ipsum</div>
+            <h1 className="h1 max-lg:h2 mb-6 uppercase text-p4 max-lg:mb-7 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+              Lorem, ipsum dolor.
+            </h1>
+            <p className="body-1 mb-14 max-w-440 max-md:mb-10">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius,
+              aspernatur?
+            </p>
+            <LinkScroll to="features" offset={-100} spy smooth>
+              Scroll
+            </LinkScroll>
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Hero;
