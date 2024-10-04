dc55556e3b0eff05f4535240dc633c70bedb4124
Dmitrii <dimterion59@gmail.com>
2024-10-04 12:19:18 +0200

    add features component

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+export const features = [
+  {
+    id: "0",
+    caption: "LOREM IPSUM",
+    title: "FEATURE 1",
+    text: "LOREM 1",
+    button: {
+      title: "Lorem ipsum",
+    },
+  },
+  {
+    id: "1",
+    caption: "LOREM IPSUM",
+    title: "FEATURE 2",
+    text: "LOREM 2",
+    button: {
+      title: "Lorem ipsum",
+    },
+  },
+];
