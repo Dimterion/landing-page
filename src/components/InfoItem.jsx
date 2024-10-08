12f8190c411d59c89389c18b38eb9fbc9de76918
Dmitrii <dimterion59@gmail.com>
2024-10-08 11:43:40 +0200

    add onClick function to infoItem component

--- src/components/InfoItem.jsx
+++ src/components/InfoItem.jsx
@@ -1,18 +1,32 @@
+import clsx from "clsx";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const InfoItem = ({ infoItem, index }) => {
+  const [activeId, setActiveId] = useState(null);
+  const active = activeId === infoItem.id;
+
   return (
     <div className="relative z-2 mb-16">
       <div
         className="group relative flex cursor-pointer items-center justify-between gap-10 px-7"
-        onClick={() => {}}
+        onClick={() => {
+          setActiveId(activeId === infoItem.id ? null : infoItem.id);
+        }}
       >
         <div className="flex-1">
           <div className="small-compact mb-1.5 text-p3 max-lg:hidden">
             {index < 10 ? "0" : ""}
             {index}
           </div>
-          <div>{infoItem.question}</div>
+          <div
+            className={clsx(
+              "h6 max-md:mih-h-20 text-p4 transition-colors duration-500 max-md:flex max-md:items-center",
+              active && "max-lg:text-p1",
+            )}
+          >
+            {infoItem.question}
+          </div>
         </div>
       </div>
     </div>
