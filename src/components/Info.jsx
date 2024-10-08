7dc03d3d0bbb52e1719657bd2e3788957b38d518
Dmitrii <dimterion59@gmail.com>
2024-10-08 11:10:07 +0200

    add logo to info component

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,4 +1,5 @@
 import { Element } from "react-scroll";
+import infoLogo from "../assets/info/info_logo.svg";
 
 const Info = () => {
   return (
@@ -14,6 +15,17 @@ const Info = () => {
             </p>
           </div>
           <div className="info-line_after absolute left-[calc(50%-1px)] top-0 -z-1 h-full w-0.5 bg-s2" />
+        </div>
+        <div className="info-glow_before relative z-2 border-2 border-s2 bg-s1">
+          <div className="container flex gap-10 max-lg:block">
+            <div className="absolute -top-10 left-[calc(50%-40px)] z-4 flex size-20 items-center justify-center rounded-half border-2 border-s2 bg-s1">
+              <img
+                src={infoLogo}
+                alt="Info section logo"
+                className="size-1/2"
+              />
+            </div>
+          </div>
         </div>
       </Element>
     </section>
